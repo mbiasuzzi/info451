ea20714c72a993d2471f40af24423469c45a323f
nadir-s <snadir177@gmail.com>
2017-04-27 15:12:38 -0400

    Added delete functionality

--- Group8MasterPage/Delete-Listing.aspx.cs
+++ Group8MasterPage/Delete-Listing.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Group8MasterPage
 {
@@ -11,7 +14,40 @@ namespace Group8MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string ConString = ConfigurationManager.ConnectionStrings["info451"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                using (var cmd = new SqlCommand("Select * from Inventory where UserID = @UserId", con))
+                {
+                    cmd.Parameters.AddWithValue("@Userid", (string)Session["Username"]);
+                    con.Open();
 
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    UserListings.DataSource = dt;
+                    UserListings.DataBind();
+                    
+                }
+            }
+        }
+
+        protected void UserListings_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            
+            string ConString = ConfigurationManager.ConnectionStrings["info451"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                using (var cmd = new SqlCommand("Delete from Inventory where Title_of_Book = @Title", con))
+                {
+                    string title = UserListings.Rows[e.RowIndex].Cells[2].Text;
+                    cmd.Parameters.AddWithValue("@Title", title);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    
+                    Response.Redirect("MyProfile.aspx");
+                }
+            }
         }
     }
 }
